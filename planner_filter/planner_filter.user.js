add8e3eb7f2b450dbae00902f46cffecd2a8e7a1
algineer <120684984+algineer@users.noreply.github.com>
2024-02-07 10:08:34 -0500

    Update filter element child creation

--- planner_filter/planner_filter.user.js
+++ planner_filter/planner_filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Planner Filter
 // @namespace    https://github.com/algineer/
-// @version      1.6.0
+// @version      1.7.0
 // @description  Allow user to filter planner tasks
 // @author       Algineer
 // @match        https://humans.ap.tesla.services/plan*
@@ -12,11 +12,29 @@
 // ==/UserScript==
 
 (function() {
-    function createElementWithProperties(tag, properties) {
+    const createElementWithProperties = (tag, properties) => {
         const element = document.createElement(tag);
 
         for (const [key, value] of Object.entries(properties)) {
-            element[key] = value;
+            if (key === 'style' && typeof value === 'object') {
+                // If the property is 'style' and the value is an object, apply styles
+                for (const [styleKey, styleValue] of Object.entries(value)) {
+                    element.style[styleKey] = styleValue;
+                }
+            } else if (key === 'children' && Array.isArray(value)) {
+                // If the property is 'children' and the value is an array of objects representing nested elements,
+                // recursively create and append each nested element to the parent
+                value.forEach(child => {
+                    if (typeof child === 'object') {
+                        const { tag: childTag, ...childProps } = child;
+                        const childElement = createElementWithProperties(childTag, childProps);
+                        element.appendChild(childElement);
+                    }
+                });
+            } else {
+                // For other properties, directly set the value
+                element[key] = value;
+            }
         }
 
         return element;
@@ -60,8 +78,18 @@
 
             const element = createElementWithProperties('label', {
                 className: 'css-1jjo2yn',
-                innerHTML: '<div class = "css-3oglug">Filter</div>' +
-                    '<input id="filter" cursor="initial" rows="4" class="css-c352py" value="">'
+                Children: [{
+                        tag: 'div',
+                        className: 'css-3oglug',
+                        innerHTML: 'Filter'
+                    },
+                    {
+                        tag: 'input',
+                        id: 'filter',
+                        className: 'css-c352py',
+                        value: ''
+                    }
+                ]
             })
             parent.append(element)
 
