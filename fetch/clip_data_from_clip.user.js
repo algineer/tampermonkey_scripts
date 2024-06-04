1ef0696d45d6e8744a3198c3324dfd09fe87e4be
algineer <120684984+algineer@users.noreply.github.com>
2024-06-04 10:59:21 -0400

    Create context menu script to fetch clip data

---
+++ fetch/clip_data_from_clip.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Clip Data
+// @namespace    https://github.com/algineer/
+// @version      1.0.0
+// @description  Console.log() clip data 
+// @author       Algineer
+// @match        https://flide.ap.tesla.services/3d/MESH*
+// @match        https://flide.ap.tesla.services/3d/TCLP*
+// @match        https://flide.ap.tesla.services/3d/FLIDE*
+// @grant        none
+// @downloadURL  https://github.com/algineer/tampermonkey_scripts/raw/main/fetch/clip_data_from_clip.user.js
+// @updateURL    https://github.com/algineer/tampermonkey_scripts/raw/main/fetch/clip_data_from_clip.user.js
+// @run-at       context-menu
+// ==/UserScript==
+
+(function() {
+
+    //fetch the data
+    const fetchData = async(id) => {
+        const response = await fetch(`/api/internal/3d/videos/${id}`)
+        const data = await response.json()
+
+        console.log(id)
+
+        return data
+    }
+
+    //process the fetched data
+    const run = async() => {
+        let id = window.location.href.match(/[a-zA-Z]+-\d\d\d\d-\d\d-\d\d-\d\d-\d\d-\d\d-......../g)[0]
+
+        let data = await fetchData(id)
+
+        console.log(data)
+    }
+
+    run()
+
+})();
