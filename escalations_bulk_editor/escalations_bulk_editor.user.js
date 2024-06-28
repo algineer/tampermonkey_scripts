317eb49349d4cb76e0b1daaa74ded02b6bae8d57
algineer <120684984+algineer@users.noreply.github.com>
2024-06-28 17:11:51 -0400

    Update escalatoins bulk editor to be more dynamic

--- escalations_bulk_editor/escalations_bulk_editor.user.js
+++ escalations_bulk_editor/escalations_bulk_editor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Escalations Bulk Editor
 // @namespace    https://github.com/algineer/
-// @version      1.0.6
+// @version      1.1.0
 // @description  Allow user to bulk edit escations (resolve/further escalte)
 // @author       Algineer
 // @match        https://*/3d/escalations*
@@ -50,7 +50,9 @@
                 b.click()
             })
 
-            setTimeout(window.location.reload(), 5000)
+            setTimeout(function() {
+                window.location.reload();
+            }, 5000);
         } else
             alert('Bulk Edit Option Needs to be Selected')
 
@@ -61,54 +63,61 @@
     requestAnimationFrame(run);
 
     function run() {
-        let parent = document.querySelector("#root > main > div > div.css-6vzluo > div > div.css-660msb > div > div.css-10p649b") //need to update now and then
-        if (parent) {
+        let starting_element = document.querySelector("[role='table']")
+        if (starting_element) {
+            let parent = starting_element.parentElement.parentElement.firstChild.firstChild.lastChild
+            let label_class = parent.lastChild.classList.value
+            let div_class = parent.lastChild.firstChild.classList.value
+            let input_class = parent.lastChild.lastChild.lastChild.lastChild.classList.value
+            let setlect_btn = parent.lastChild.firstChild.lastChild.classList.value
+                // let parent = document.querySelector("div.css-199fc11") //need to update now and then
+            if (parent) {
 
-            const element = createElementWithProperties('label', {
-                className: 'css-1sr1772',
-                children: [{
-                        tag: 'div',
-                        className: 'css-1xhs0h5',
-                        innerHTML: 'Bulk Edit Escalations'
-                    },
-                    {
-                        tag: 'div',
-                        className: ' css-1b72rnb-control',
-                        children: [{
-                                tag: 'select',
-                                id: 'bulkEditOption',
-                                className: 'css-1vlq6kh',
-                                style: {
-                                    height: '30px',
-                                },
-                                innerHTML: '<option value="selectOption">Select Option</option>' +
-                                    '<option value="Resolve">Resolve</option>' +
-                                    '<option value="Further Escalate">Further Escalate</option>'
+                const element = createElementWithProperties('label', {
+                    className: label_class,
+                    children: [{
+                            tag: 'div',
+                            className: div_class,
+                            innerHTML: 'Bulk Edit Escalations'
+                        },
+                        {
+                            tag: 'div',
+                            className: input_class,
+                            children: [{
+                                    tag: 'select',
+                                    id: 'bulkEditOption',
+                                    className: setlect_btn,
+                                    style: {
+                                        height: '30px',
+                                    },
+                                    innerHTML: '<option value="selectOption">Select Option</option>' +
+                                        '<option value="Resolve">Resolve</option>' +
+                                        '<option value="Further Escalate">Further Escalate</option>'
 
-                            },
-                            {
-                                tag: 'button',
-                                id: 'bulkBtn',
-                                className: 'css-1vlq6kh',
-                                style: {
-                                    height: '30px',
                                 },
-                                title: 'Window will reload after completion',
-                                innerText: '->',
-                                onclick: () => {
-                                    bulkEdit()
+                                {
+                                    tag: 'button',
+                                    id: 'bulkBtn',
+                                    className: setlect_btn,
+                                    style: {
+                                        height: '30px',
+                                    },
+                                    title: 'Window will reload after completion',
+                                    innerText: '->',
+                                    onclick: () => {
+                                        bulkEdit()
+                                    }
                                 }
-                            }
-                        ]
+                            ]
 
-                    }
-                ]
-            });
-            parent.append(element)
+                        }
+                    ]
+                });
+                parent.append(element)
 
-            //change any parent styles
-            parent.style.gridTemplateColumns = '500px 500px 153px'
-
+                //change any parent styles
+                parent.style.gridTemplateColumns = '500px 500px 175px'
+            }
         } else
             requestAnimationFrame(run);
     }
