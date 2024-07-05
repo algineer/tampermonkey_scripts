fe02da46f45786b3e66e19f447ea6070e1a7bde0
algineer <120684984+algineer@users.noreply.github.com>
2024-07-05 11:18:28 -0400

    Upadate domain path to support multiple endpoints, id regex

--- fetch/clip_data_from_clip.user.js
+++ fetch/clip_data_from_clip.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Clip Data
 // @namespace    https://github.com/algineer/
-// @version      1.0.1
+// @version      1.1.0
 // @description  Console.log() clip data 
 // @author       Algineer
 // @match        https://*/3d/MESH*
 // @match        https://*/3d/TCLP*
 // @match        https://*/3d/FLDE*
+// @match        https://*/viz/tclip*
 // @grant        none
 // @downloadURL  https://github.com/algineer/tampermonkey_scripts/raw/main/fetch/clip_data_from_clip.user.js
 // @updateURL    https://github.com/algineer/tampermonkey_scripts/raw/main/fetch/clip_data_from_clip.user.js
@@ -16,8 +17,8 @@
 (function() {
 
     //fetch the data
-    const fetchData = async(id) => {
-        const response = await fetch(`/api/internal/3d/videos/${id}`)
+    const fetchData = async(path, id) => {
+        const response = await fetch(`${path}${id}`)
         const data = await response.json()
 
         console.log(id)
@@ -27,13 +28,18 @@
 
     //process the fetched data
     const run = async() => {
-        let id = window.location.href.match(/[a-zA-Z]+-\d\d\d\d-\d\d-\d\d-\d\d-\d\d-\d\d-......../g)[0]
+        let domain = window.location.href.match(/(?<=\/\/)(.*?)(?=\.)/)[0]
+        let id = window.location.href.match(/[a-zA-Z]+(?:-\d+){6}-[[:alnum:]]+/g)[0]
+        let data
 
-        let data = await fetchData(id)
+        if (domain == 'flide')
+            data = await fetchData('/api/internal/3d/videos/', id)
+        else if (domain == 'tclip')
+            data = await fetchData('/api/internal/v1/clips/', id)
 
         window.data = data
 
-        console.log(window.data)
+        console.log(data)
     }
 
     run()
